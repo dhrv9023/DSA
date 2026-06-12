45204db14afa83778f8856fdfd47d09aae4651d1
dhrv9023 <dhrv9023@gmail.com>
2026-06-12 22:04:43 +0530

    changes

---
+++ ARRAYS/easy/largest_element.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> arr = {3,2,1,5,2};
+    int largest = 0;
+    for(int i =0;i<arr.size()-1;i++){
+        if (largest < arr[i]){
+            largest = arr[i];
+        }
+    }
+    cout<<largest;
+}
